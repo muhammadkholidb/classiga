41189bcdab59e152a89a6c41bc7c9db73303a943
Muhammad Kholid B <muhammadkholidb@gmail.com>
2018-07-14 08:23:49 +0700

    Add gitlab-ci configuration Fix bean definition error on class attribute cannot use values from properties, use factory bean instead

---
+++ classiga-commons/src/main/java/ga/classi/commons/helper/ObjectFactoryBean.java
@@ -0,0 +1,29 @@
+package ga.classi.commons.helper;
+
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+
+/**
+ *
+ * @author muhammad
+ */
+public class ObjectFactoryBean extends AbstractFactoryBean<Object> {
+    
+    // To read https://stackoverflow.com/questions/2209010/is-it-possible-to-specify-a-class-name-for-spring-framework-in-an-external-file
+    
+    private Class targetClass;
+    
+    @Override
+    public Class<?> getObjectType() {
+        return targetClass;
+    }
+
+    @Override
+    protected Object createInstance() throws Exception {
+        return targetClass.newInstance();
+    }
+
+    public void setTargetClass(Class targetClass) {
+        this.targetClass = targetClass;
+    }
+    
+}
